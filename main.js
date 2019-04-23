ffc441a5ae7ffc3ff284353cc910a2b018391d65
Miguel Castro <miguelpdecastro@gmail.com>
2019-04-23 12:41:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const StaySchedule = require('./StaySchedule');
 const GASchedulePlanner = require('./GASchedulePlanner');
 
-let chromosomeSize=42;
+let chromosomeSize=49;
 let populationSize=500;
 let maxGeneration=75;
 let eliteSize=5;
@@ -12,28 +12,29 @@ let planner = new GASchedulePlanner(chromosomeSize, populationSize, maxGeneratio
 // Rule 1: Can sleep at her parent's house any weekday;
 
 // Rule 2: Can sleep at my father's house between friday and sunday only;
-let weightViolationsOfRule2=4.0; //2
+let weightViolationsOfRule2=4.0;
 
 // Rule 3: Can only sleep apart between monday and friday;
-let weightViolationsOfRule3=1; //0.5
+let weightViolationsOfRule3=1;
 
 // Rule 4: It can't be spent more than 3 consecutive days in the same configuration, 
 // which are: sleeping at her parent's house, sleeping at my father's house or sleeping apart;
-let weightViolationsOfRule4=2; //3
+let weightViolationsOfRule4=2;
 
 // Rule 5: If n consecutive days are spent at the same configuration, then this configuration 
 // can't be used for n+1 consecutive days;
-let weightViolationsOfRule5=2; //1
+let weightViolationsOfRule5=2;
 
 //Preference 1: We'd like to spent the least number of days possible sleeping apart;
-let weightViolationsOfPreference1=2; //2
+let weightViolationsOfPreference1=2;
 
 // Preference 2: We'd like to keep the number of switches between configurations as low 
 // as possible. Switching betweeen sleeping at any of the houses is worst then switching 
 // from sleeping apart to sleeping at any of the houses and vice versa.
-let weightViolationsOfPreference2=0.5; //1
+let weightViolationsOfPreference2=0.5;
 
-planner.model='mpp---mm----xpp-----mm----xpp-----mm----xp';
+planner.model='mpppxmmmpxxxpppp----mm----xpp-----mm----xp'; //'mppp--mm----xpp-----mm----xpp-----mm----xp'
+
 planner.weights= [
     weightViolationsOfRule2,
     weightViolationsOfRule3,
